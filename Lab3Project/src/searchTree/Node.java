879631d1249985112e44d7b54e3b2409fdfae648
natashajyoti <133975055+natashajyoti@users.noreply.github.com>
2023-07-24 02:24:20 +0530

    Add files via upload

---
+++ Lab3Project/src/searchTree/Node.java
@@ -0,0 +1,16 @@
+package searchTree;
+
+public class Node {
+	Node left;
+	int data;
+	Node right;
+	
+	public Node(int data)
+	{
+		left=null;
+		this.data=data;
+		right=null;
+		
+	}
+
+}
---
+++ Lab3Project/src/stack/MyStack.java
@@ -0,0 +1,44 @@
+package stack;
+
+import java.util.Stack;
+
+import javax.xml.stream.events.Characters;
+
+public class MyStack {
+
+	Stack<Character> stack = new Stack<>();
+
+	public Boolean checkBracket(String input) {
+		char z,p;
+		for (int i = 0; i < input.length(); i++) {
+			z = input.charAt(i);
+			if (z=='{'|| z=='['||z=='(')
+			{
+				stack.push(z);
+				continue;
+			}
+
+			if (stack.isEmpty())
+
+				return false;
+
+			switch(z)
+			{
+			case'}': p=stack.pop();
+			if (p=='['||p=='(')
+				return false;
+			break;
+			case']': p=stack.pop();
+			if (p=='{'|| p=='(')
+				return false;
+			break;
+			case')': p=stack.pop();
+			if (p=='{'|| p=='[')
+				return false;
+			break;
+			}
+		}
+		return (stack.isEmpty());
+	}
+
+}
